c2462bb1ee4f662b8943efb1c0f396ec313b8d02
The Geek Viking <c.sergerie@hotmail.fr>
2024-11-17 17:08:18 -0500

    Site Map added

---
+++ src/data/serviceDetails.ts
@@ -0,0 +1,18 @@
+// src/data/serviceDetails.ts
+export const serviceDetails = {
+  "debouchage-drain": {
+    title: "Débouchage de Drain | Service 24/7",
+    description: "We provide emergency drain unclogging services 24/7.",
+    imgUrl: "/images/drain.jpg",
+  },
+  "debouchage-tuyaux": {
+    title: "Débouchage de Tuyaux | Service 24/7",
+    description: "Expert pipe unclogging services for your home or business.",
+    imgUrl: "/images/sainte-julie.jpg",
+  },
+  "debouchage-toilette": {
+    title: "Débouchage de Toilette | Service 24/7",
+    description: "Expert toilet unclogging services for your home or business.",
+    imgUrl: "/images/saint-jean.jpg",
+  },
+};
---
+++ src/scripts/menu.js
@@ -0,0 +1,33 @@
+const btnHamburger = document.getElementById("btn-hamburger");
+const mobileMenu = document.getElementById("mobile-menu");
+const btnNavbarClose = document.getElementById("navbar-close");
+const navItems = document.querySelectorAll("#mobile-menu a"); // Select all nav items
+const servicesBtn = document.getElementById("servicesBtn");
+const servicesDropDown = document.getElementById("servicesDropDown");
+const mobileServicesBtn = document.getElementById("mobileServicesBtn");
+const mobileservicesDropDown = document.getElementById(
+  "mobileservicesDropDown"
+);
+
+btnNavbarClose.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
+
+btnHamburger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
+
+// Close mobile menu when a nav item is clicked
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    mobileMenu.classList.add("hidden"); // Ensure the menu closes
+  });
+});
+
+servicesBtn.addEventListener("click", () => {
+  servicesDropDown.classList.toggle("hidden");
+});
+
+mobileServicesBtn.addEventListener("click", () => {
+  mobileservicesDropDown.classList.toggle("hidden");
+});
