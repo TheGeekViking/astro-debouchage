e2b5249c1b99905ec32e372d07988532ce696056
The Geek Viking <c.sergerie@hotmail.fr>
2024-11-19 23:11:16 -0500

    Pages remove

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,12 @@ import tailwind from "@astrojs/tailwind";
 
 import netlify from "@astrojs/netlify";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
+  site: "https://yourdomain.com",
   output: "hybrid",
   adapter: netlify(),
 });
--- src/data/serviceDetails.ts
+++ src/data/serviceDetails.ts
@@ -2,17 +2,235 @@
 export const serviceDetails = {
   "debouchage-drain": {
     title: "Débouchage de Drain | Service 24/7",
-    description: "We provide emergency drain unclogging services 24/7.",
-    imgUrl: "/images/drain.jpg",
-  },
-  "debouchage-tuyaux": {
-    title: "Débouchage de Tuyaux | Service 24/7",
-    description: "Expert pipe unclogging services for your home or business.",
-    imgUrl: "/images/sainte-julie.jpg",
+    MainTitle: "Service de débouchage de drain",
+    firstDescription: "We provide emergency drain unclogging services 24/7.",
+    imgUrl1: "/images/drain.jpg",
+    secondTitle: "Le drain va deboucher",
+    secondDescription: "We provide emergency drain unclogging services 24/7.",
+    details: [
+      {
+        title: "Push to deploy.",
+        description:
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      },
+      {
+        title: "SSL certificates.",
+        description:
+          "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
+      },
+      {
+        title: "Database backups.",
+        description:
+          "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+      },
+    ],
+    thirdTitle: "Le drain va deboucher",
+    thirdDescription:
+      "We provide emergency drain unclogging services 24/7.<br/> We provide emergency drain unclogging services 24/7.",
   },
   "debouchage-toilette": {
     title: "Débouchage de Toilette | Service 24/7",
-    description: "Expert toilet unclogging services for your home or business.",
-    imgUrl: "/images/saint-jean.jpg",
+    MainTitle: "Service de débouchage de toilette",
+    firstDescription:
+      "Expert toilet unclogging services for your home or business.",
+    imgUrl1: "/images/saint-jean.jpg",
+    secondTitle: "Le drain va deboucher",
+    secondDescription: "We provide emergency toilet unclogging services 24/7.",
+    details: [
+      {
+        title: "Push to deploy.",
+        description:
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      },
+      {
+        title: "SSL certificates.",
+        description:
+          "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
+      },
+      {
+        title: "Database backups.",
+        description:
+          "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+      },
+    ],
+    thirdTitle: "Le drain va deboucher",
+    thirdDescription:
+      "We provide emergency drain unclogging services 24/7.<br/> We provide emergency drain unclogging services 24/7.",
+  },
+  "debouchage-evier-cuisine": {
+    title: "Débouchage d'évier de cuisine | Service 24/7",
+    MainTitle: "Service de débouchage d'évier de cuisine",
+    firstDescription:
+      "Expert pipe unclogging services for your home or business.",
+    imgUrl1: "/images/sainte-julie.jpg",
+    secondTitle: "Le drain va deboucher",
+    secondDescription: "We provide emergency sink unclogging services 24/7.",
+    details: [
+      {
+        title: "Push to deploy.",
+        description:
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      },
+      {
+        title: "SSL certificates.",
+        description:
+          "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
+      },
+      {
+        title: "Database backups.",
+        description:
+          "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+      },
+    ],
+    thirdTitle: "Le drain va deboucher",
+    thirdDescription:
+      "We provide emergency drain unclogging services 24/7.<br/> We provide emergency drain unclogging services 24/7.",
+  },
+  "debouchage-bain-douche": {
+    title: "Débouchage de bain et douche | Service 24/7",
+    MainTitle: "Service de débouchage de bain et douche",
+    firstDescription:
+      "Expert pipe unclogging services for your home or business.",
+    imgUrl1: "/images/sainte-julie.jpg",
+    secondTitle: "Le drain va deboucher",
+    secondDescription:
+      "We provide emergency bath and shower unclogging services 24/7.",
+    details: [
+      {
+        title: "Push to deploy.",
+        description:
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      },
+      {
+        title: "SSL certificates.",
+        description:
+          "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
+      },
+      {
+        title: "Database backups.",
+        description:
+          "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+      },
+    ],
+    thirdTitle: "Le drain va deboucher",
+    thirdDescription:
+      "We provide emergency drain unclogging services 24/7.<br/> We provide emergency drain unclogging services 24/7.",
+  },
+  "debouchage-lavabo": {
+    title: "Débouchage de lavabo | Service 24/7",
+    MainTitle: "Service de débouchage de lavabo",
+    firstDescription:
+      "Expert pipe unclogging services for your home or business.",
+    imgUrl1: "/images/sainte-julie.jpg",
+    secondTitle: "Le drain va deboucher",
+    secondDescription: "We provide emergency pipes unclogging services 24/7.",
+    details: [
+      {
+        title: "Push to deploy.",
+        description:
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      },
+      {
+        title: "SSL certificates.",
+        description:
+          "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
+      },
+      {
+        title: "Database backups.",
+        description:
+          "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+      },
+    ],
+    thirdTitle: "Le drain va deboucher",
+    thirdDescription:
+      "We provide emergency drain unclogging services 24/7.<br/> We provide emergency drain unclogging services 24/7.",
+  },
+  "nettoyage-drain-hydro-pression": {
+    title: "Nettoyage à hydro-pression | Service 24/7",
+    MainTitle: "Service de nettoyage à hydro-pression",
+    firstDescription:
+      "Expert pipe unclogging services for your home or business.",
+    imgUrl1: "/images/sainte-julie.jpg",
+    secondTitle: "Le drain va deboucher",
+    secondDescription: "We provide emergency hydro-jet cleaning services 24/7.",
+    details: [
+      {
+        title: "Push to deploy.",
+        description:
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      },
+      {
+        title: "SSL certificates.",
+        description:
+          "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
+      },
+      {
+        title: "Database backups.",
+        description:
+          "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+      },
+    ],
+    thirdTitle: "Le drain va deboucher",
+    thirdDescription:
+      "We provide emergency drain unclogging services 24/7.<br/> We provide emergency drain unclogging services 24/7.",
+  },
+  "nettoyage-drain-garage": {
+    title: "Nettoyage de drain de garage | Service 24/7",
+    MainTitle: "Service de nettoyage de drain de garage",
+    firstDescription:
+      "Expert pipe unclogging services for your home or business.",
+    imgUrl1: "/images/sainte-julie.jpg",
+    secondTitle: "Le drain va deboucher",
+    secondDescription:
+      "We provide emergency garage drain unclogging services 24/7.",
+    details: [
+      {
+        title: "Push to deploy.",
+        description:
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      },
+      {
+        title: "SSL certificates.",
+        description:
+          "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
+      },
+      {
+        title: "Database backups.",
+        description:
+          "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+      },
+    ],
+    thirdTitle: "Le drain va deboucher",
+    thirdDescription:
+      "We provide emergency drain unclogging services 24/7.<br/> We provide emergency drain unclogging services 24/7.",
+  },
+  "inspection-camera": {
+    title: "Inspection par caméra | Service 24/7",
+    MainTitle: "Service d'inspection par caméra",
+    firstDescription:
+      "Expert pipe unclogging services for your home or business.",
+    imgUrl1: "/images/sainte-julie.jpg",
+    secondTitle: "Le drain va deboucher",
+    secondDescription: "We provide camera inspection services 24/7.",
+    details: [
+      {
+        title: "Push to deploy.",
+        description:
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      },
+      {
+        title: "SSL certificates.",
+        description:
+          "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
+      },
+      {
+        title: "Database backups.",
+        description:
+          "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+      },
+    ],
+    thirdTitle: "Le drain va deboucher",
+    thirdDescription:
+      "We provide emergency drain unclogging services 24/7.<br/> We provide emergency drain unclogging services 24/7.",
   },
 };
