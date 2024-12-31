a797fe6a49e23a792b037bccd3e8094975e6acd5
The Geek Viking <c.sergerie@hotmail.fr>
2024-12-30 23:33:28 -0500

    Major pages added

--- src/data/cityDetails.ts
+++ src/data/cityDetails.ts
@@ -4,11 +4,11 @@ export const cityDetails = {
     metaDescription:
       "Service d'urgence de débouchage à Montréal: drains, lavabos, toilettes et plus. Intervention rapide 24h/7 avec des plombiers qualifiés. Satisfaction garantie!",
     cardTitle: "Débouchage Montreal",
+    cardImgUrl: "../images/montreal.jpg",
     mainTitle: "Service de débouchage à Montreal",
     description:
       "Besoin d'un service d'urgence de débouchage à Montréal? Notre équipe de plombiers offrent un service d'intervention, disponible 24h/7, pour toute vos urgences de plomberie. Peu importe que le lavabo soit bouché, un drain obstrué ou le besoin d'un nettoyage de drain, nous intervenons partout sur l'île de Montréal.",
-    imgUrl:
-      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+    imgUrl: "../images/montreal.jpg",
     secondSubTitle: "Disponibilité",
     secondTitle: "Service d'urgence rapide à Montréal, 24h/7",
     secondDescription:
@@ -34,6 +34,158 @@ export const cityDetails = {
     thirdTitle: "Ce qui nous démarque des autres à Montréal",
     thirdDescription:
       "Chez Débouchage Solution, chaque urgence est prise au sérieux. Nous savons à quel point un problème de plomberie peut être une source de stresse et c'est pour cela que nous offrons le service de débouchage le plus rapide partout sur l'île de Montréal.",
+  },
+  // Richelieu
+  richelieu: {
+    title: "Débouchage Richelieu | Urgence 24/7",
+    metaDescription:
+      "Besoin d’un débouchage à Richelieu? Nos plombiers qualifiés interviennent rapidement 24h/7 pour résoudre vos urgences de drains et canalisations bouchés. Appelez dès maintenant!",
+    cardTitle: "Débouchage Richelieu",
+    cardImgUrl: "../images/richelieu.jpg",
+    mainTitle: "Service de débouchage à Richelieu",
+    description:
+      "Besoin d’un plombier local pour un débouchage à Richelieu ? Nous offrons un service d’urgence 24h/7, idéal pour résoudre rapidement vos problèmes de plomberie. Avec une équipe experte et des outils modernes, nous intervenons rapidement pour garantir des solutions fiables et durables dans toute la région de Richelieu.",
+    imgUrl: "../images/richelieu.jpg",
+    secondSubTitle: "Disponibilité",
+    secondTitle: "Débouchage d’Urgence Rapide à Richelieu, Disponible 24h/7",
+    secondDescription:
+      "Nos plombiers locaux offrent bien plus qu’un simple débouchage. Grâce à des outils modernes et des techniques éprouvées, nous intervenons rapidement pour résoudre tous types de blocages. Que ce soit pour un drain obstrué, un lavabo bouché, ou une toilette qui déborde, nous sommes là pour vous aider 24h/7.",
+    details: [
+      {
+        title: "Intervention Rapide et Efficace",
+        description:
+          "Nous arrivons la journée même pour résoudre vos urgences, afin que vous retrouviez rapidement votre confort.",
+      },
+      {
+        title: "Expertise Locale à Richelieu",
+        description:
+          "Nos plombiers connaissent les infrastructures locales, garantissant un service adapté à vos besoins spécifiques.",
+      },
+      {
+        title: "Équipements Modernes et Résultats Durables",
+        description:
+          "Nous utilisons des outils de pointe pour assurer un débouchage précis et prévenir les problèmes futurs.",
+      },
+    ],
+    thirdSubTitle: "Expertise",
+    thirdTitle: "Des Plombiers Dédiés à Vos Besoins à Richelieu",
+    thirdDescription:
+      "Chez Débouchage Solutions, nous mettons votre satisfaction au cœur de nos priorités. Nos plombiers spécialisés à Richelieu sont à votre porté et disponibles 24h/7 pour intervenir rapidement, avec des solutions fiables et adaptées à chaque situation. Choisissez un service qui garantit confort et tranquillité d’esprit.",
+  },
+  // Saint-Basile-le-Grand
+  "saint-basile-le-grand": {
+    title: "Débouchage Saint-Basile-le-Grand | Urgence 24/7",
+    metaDescription:
+      "Service de débouchage à Saint-Basile-le-Grand. Plombiers locaux disponibles 24h/7 pour des interventions rapides et fiables. Contactez-nous dès maintenant!",
+    cardTitle: "Débouchage Saint-Basile-le-Grand",
+    cardImgUrl: "../images/saint-basile.jfif",
+    mainTitle: "Service de débouchage à Saint-Basile-le-Grand",
+    description:
+      "À Saint-Basile-le-Grand, notre équipe de plombiers qualifiés offre un service rapide et fiable pour tous vos besoins de débouchage. Que ce soit pour un drain obstrué, un lavabo bouché ou une urgence de plomberie, nous intervenons 24h/7 avec des solutions durables et efficaces. Appelez dès aujourd’hui pour une assistance immédiate dans votre région.",
+    imgUrl: "../images/saint-basile.jfif",
+    secondSubTitle: "Disponibilité",
+    secondTitle:
+      "Des Solutions de Débouchage Personnalisées pour Saint-Basile-le-Grand",
+    secondDescription:
+      "Chaque problème de plomberie est unique, c’est pourquoi nos plombiers à Saint-Basile-le-Grand proposent des solutions adaptées à vos besoins. Avec une approche sur mesure et des outils avancés, nous assurons un service rapide, fiable, et efficace pour garantir des résultats durables.",
+    details: [
+      {
+        title: "Évaluation Rapide et Transparente",
+        description:
+          "Avant chaque intervention, nous analysons la situation pour vous offrir une solution claire et un devis sans surprises.",
+      },
+      {
+        title: "Service Local et Approche Personnalisée",
+        description:
+          "En tant que plombiers de la région, nous comprenons vos besoins spécifiques et proposons des interventions adaptées à votre quartier.",
+      },
+      {
+        title: "Techniques Modernes et Écologiques",
+        description:
+          "Nous utilisons des méthodes respectueuses de l’environnement pour déboucher vos canalisations sans endommager vos installations.",
+      },
+    ],
+    thirdSubTitle: "Expertise",
+    thirdTitle:
+      "Pourquoi Faire Confiance à Nos Services à Saint-Basile-le-Grand?",
+    thirdDescription:
+      "À Saint-Basile-le-Grand, nous sommes votre solution de confiance pour tous vos besoins de débouchage. Que ce soit pour un lavabo bouché, des canalisations obstruées, ou un nettoyage à hydro-pression, notre équipe intervient rapidement pour résoudre vos urgences. Disponibles 24h/7, nos plombiers spécialisés utilisent des équipements modernes pour garantir des résultats fiables et durables, tout en offrant des solutions économiques adaptées à votre budget.",
+  },
+  // Saint-Bruno-de-Montarville
+  "saint-bruno-de-montarville": {
+    title: "Débouchage Saint-Bruno-de-Montarville | Urgence 24/7",
+    metaDescription:
+      "Débouchage professionnel à Saint-Bruno-de-Montarville. Plombiers d’urgence disponibles 24h/7 pour drains, lavabos, et canalisations bouchés. Service rapide, durable et adapté à vos besoins!",
+    cardTitle: "Débouchage Saint-Bruno-de-Montarville",
+    cardImgUrl: "../images/saint-bruno.jpg",
+    mainTitle: "Service de débouchage à Saint-Bruno-de-Montarville",
+    description:
+      "À Saint-Bruno-de-Montarville, nous savons que les urgences de plomberie ne peuvent pas attendre. C’est pourquoi notre équipe de plombiers d’urgence est disponible 24h/7 pour intervenir rapidement et efficacement. Que vous ayez un drain obstrué, une canalisation bouchée ou un lavabo bloqué, nous utilisons des techniques modernes pour garantir des résultats durables et un service adapté à vos besoins. Faites appel à nos experts locaux dès aujourd’hui pour une intervention rapide et sans tracas.",
+    imgUrl: "../images/saint-bruno.jpg",
+    secondSubTitle: "Engagement Local",
+    secondTitle:
+      "Un Service de Débouchage Adapté aux Besoins de Saint-Bruno-de-Montarville",
+    secondDescription:
+      "Nos plombiers spécialisés à Saint-Bruno-de-Montarville comprennent les particularités des infrastructures locales. Grâce à notre expertise et à nos équipements avancés, nous proposons des solutions rapides et efficaces pour tous vos problèmes de drains et canalisations bouchés.",
+    details: [
+      {
+        title: "Expertise Locale",
+        description:
+          "Nous connaissons les spécificités des installations à Saint-Bruno-de-Montarville, garantissant des interventions adaptées à votre région.",
+      },
+      {
+        title: "Disponibilité 24h/7 pour Vos Urgences",
+        description:
+          "Notre équipe est prête à intervenir à tout moment pour répondre à vos besoins en débouchage et dépannage.",
+      },
+      {
+        title: "Équipement Moderne pour Résultats Durables",
+        description:
+          "Nous utilisons des outils de pointe pour résoudre vos problèmes rapidement, tout en assurant la durabilité des solutions.",
+      },
+    ],
+    thirdSubTitle: "choix de la qualité",
+    thirdTitle: "Pourquoi Choisir Nos Services à Saint-Bruno-de-Montarville?",
+    thirdDescription:
+      "À Saint-Bruno-de-Montarville, nous sommes plus qu’un simple service de débouchage – nous sommes vos partenaires de confiance pour résoudre vos urgences de plomberie. Avec une équipe de plombiers expérimentés, une disponibilité 24h/7, et un engagement envers des solutions fiables et rapides, nous veillons à ce que chaque intervention soit une expérience sans stress. Faites le choix de la qualité et de la tranquillité d’esprit pour vos besoins en débouchage.",
+  },
+  // Saint-Constant
+  "saint-constant": {
+    title: "Débouchage Saint-Constant | Urgence 24/7",
+    metaDescription:
+      "Service de débouchage à Saint-Constant. Plombiers locaux disponibles 24h/7 pour des interventions rapides et fiables. Drains, lavabos, canalisations bouchées ? Appelez dès aujourd’hui!",
+    cardTitle: "Débouchage Saint-Constant",
+    cardImgUrl: "../images/saint-constant.jpg",
+    mainTitle: "Service de débouchage à Saint-Constant",
+    description:
+      "À Saint-Constant, notre équipe de plombiers locaux spécialisés est disponible 24h/7 pour intervenir rapidement en cas d’urgence. Nous offrons un service de débouchage professionnel adapté à tous vos besoins, que ce soit pour un drain bloqué, une canalisation obstruée ou une réparation urgente. Faites confiance à nos experts pour des solutions fiables, durables et réalisées avec soin dans votre région.",
+    imgUrl: "../images/saint-constant.jpg",
+    secondSubTitle: "Engagement Local",
+    secondTitle: "Des Solutions Fiables pour Vos Urgences à Saint-Constant",
+    secondDescription:
+      "À Saint-Constant, nous comprenons l’importance d’une intervention rapide et adaptée. Nos plombiers locaux offrent des solutions personnalisées pour répondre efficacement à tous vos besoins en débouchage. Avec une approche axée sur la qualité et des équipements de pointe, nous garantissons des résultats qui durent.",
+    details: [
+      {
+        title: "Solutions Sur-Mesure",
+        description:
+          "Chaque situation est unique, c’est pourquoi nous adaptons nos méthodes à vos besoins spécifiques, assurant un débouchage efficace et durable.",
+      },
+      {
+        title: "Expertise Locale Inégalée",
+        description:
+          "Basés à Saint-Constant, nos plombiers connaissent parfaitement les infrastructures locales, vous garantissant un service rapide et précis.",
+      },
+      {
+        title: "Engagement pour Votre Satisfaction",
+        description:
+          "Votre satisfaction est notre priorité. Nous nous assurons que chaque intervention soit réalisée avec soin et professionnalisme.",
+      },
+    ],
+    thirdSubTitle: "Expertise",
+    thirdTitle:
+      "Votre Partenaire de Confiance à Saint-Constant pour un Service de Qualité",
+    thirdDescription:
+      "À Saint-Constant, nous ne nous contentons pas de déboucher vos canalisations – nous offrons une expérience client exceptionnelle. Grâce à notre équipe de plombiers locaux, à des outils modernes, et à une approche centrée sur vos besoins, nous garantissons des résultats fiables et une intervention rapide, à chaque fois. Faites confiance à notre expertise pour un service professionnel, adapté et sans compromis.",
   },
   // Add more cities here
 };
