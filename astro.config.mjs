4bab7bb823a388ec05b79a98480ce441a9e07b4d
The Geek Viking <c.sergerie@hotmail.fr>
2025-09-18 19:15:18 -0400

    Put back the adapter for testing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-// import netlify from "@astrojs/netlify";
+import netlify from "@astrojs/netlify";
 
 import sitemap from "@astrojs/sitemap";
 
@@ -12,5 +12,5 @@ export default defineConfig({
   integrations: [tailwind(), sitemap()],
   site: "https://debouchagesolutions.ca",
   output: "static",
-  // adapter: netlify(),
+  adapter: netlify(),
 });
